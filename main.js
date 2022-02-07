6d22e7444b9fafc0d45122d35f425d1e07e2f22e
Harshithalekkala <80406614+Harshithalekkala@users.noreply.github.com>
2022-02-07 18:34:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,39 @@ function setup(){
 canvas=createCanvas(280, 280);
 canvas.center();
 background("white");
+canvas.mouseReleased(classifyCanvas);
+synth = window.speechsynthesis;
 }
 
 function clearCanvas() {
     background("white"); 
+}
+
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw() {
+  strokeWeight(13);
+  stroke(0);
+  if (mouseIsPressed) {
+      line(pmouseX, pmouseY, mouseX, mouseY)
+  }  
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTMl = 'label:' +  results[0].label;
+     
+    document.getElementById('confidence').innerHTMl = 'confidence:' +  Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = newSpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
 }
